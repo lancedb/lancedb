5d7832c8a5d4f55bc98a4251ffe52ccc482ba362
Chang She <759245+changhiskhan@users.noreply.github.com>
2023-03-24 18:16:29 -0700

    update for release

---
+++ python/tests/test_embeddings.py
@@ -0,0 +1,36 @@
+#  Copyright 2023 LanceDB Developers
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import numpy as np
+import pyarrow as pa
+
+from lancedb.embeddings import with_embeddings
+
+
+def mock_embed_func(input_data):
+    return [np.random.randn(128).tolist() for _ in range(len(input_data))]
+
+
+def test_with_embeddings():
+    data = pa.Table.from_arrays(
+        [
+            pa.array(["foo", "bar"]),
+            pa.array([10.0, 20.0]),
+        ],
+        names=["text", "price"],
+    )
+    data = with_embeddings(mock_embed_func, data)
+    assert data.num_columns == 3
+    assert data.num_rows == 2
+    assert data.column_names == ["text", "price", "vector"]
+    assert data.column("text").to_pylist() == ["foo", "bar"]
+    assert data.column("price").to_pylist() == [10.0, 20.0]
