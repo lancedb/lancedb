59229d9ae1287ca0961aa226398c469bd97b6c60
Chang She <759245+changhiskhan@users.noreply.github.com>
2023-03-24 19:45:46 -0700

    fix 3.11

--- python/tests/test_embeddings.py
+++ python/tests/test_embeddings.py
@@ -10,6 +10,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import sys
+
 import numpy as np
 import pyarrow as pa
 
@@ -21,16 +23,20 @@ def mock_embed_func(input_data):
 
 
 def test_with_embeddings():
-    data = pa.Table.from_arrays(
-        [
-            pa.array(["foo", "bar"]),
-            pa.array([10.0, 20.0]),
-        ],
-        names=["text", "price"],
-    )
-    data = with_embeddings(mock_embed_func, data)
-    assert data.num_columns == 3
-    assert data.num_rows == 2
-    assert data.column_names == ["text", "price", "vector"]
-    assert data.column("text").to_pylist() == ["foo", "bar"]
-    assert data.column("price").to_pylist() == [10.0, 20.0]
+    for wrap_api in [True, False]:
+        if wrap_api and sys.version_info.minor >= 11:
+            # ratelimiter package doesn't work on 3.11
+            continue
+        data = pa.Table.from_arrays(
+            [
+                pa.array(["foo", "bar"]),
+                pa.array([10.0, 20.0]),
+            ],
+            names=["text", "price"],
+        )
+        data = with_embeddings(mock_embed_func, data, wrap_api=wrap_api)
+        assert data.num_columns == 3
+        assert data.num_rows == 2
+        assert data.column_names == ["text", "price", "vector"]
+        assert data.column("text").to_pylist() == ["foo", "bar"]
+        assert data.column("price").to_pylist() == [10.0, 20.0]
