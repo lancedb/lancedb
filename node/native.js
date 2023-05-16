1fa160e8314571386c33cc494208d3b2fbf36950
gsilvestrin <gc@eto.ai>
2023-05-16 16:40:43 -0700

    fixing native.js

--- node/native.js
+++ node/native.js
@@ -12,29 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-<<<<<<<< HEAD:node/examples/ts/src/index.ts
-import * as vectordb from 'vectordb';
-
-async function example () {
-    const db = await vectordb.connect('data/sample-lancedb')
-
-    const data = [
-        { id: 1, vector: [0.1, 0.2], price: 10 },
-        { id: 2, vector: [1.1, 1.2], price: 50 }
-    ]
-
-    const table = await db.createTable('vectors', data)
-    console.log(await db.tableNames())
-
-    const results = await table
-        .search([0.1, 0.3])
-        .limit(20)
-        .execute()
-    console.log(results)
-}
-
-example().then(_ => { console.log ("All done!") })
-========
 let nativeLib;
 
 if (process.platform === "darwin" && process.arch === "arm64") {
@@ -46,4 +23,4 @@ if (process.platform === "darwin" && process.arch === "arm64") {
 }
 
 module.exports = nativeLib
->>>>>>>> gsilvestrin/nodejs_linux_1:node/native.js
+
