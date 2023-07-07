d157d2afde243151c74e1bd938a50df7428aeb98
Lei Xu <eddyxu@gmail.com>
2023-07-07 14:39:39 -0700

    fix isort /black

--- python/lancedb/remote/db.py
+++ python/lancedb/remote/db.py
@@ -11,8 +11,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from urllib.parse import urlparse
 from typing import List
+from urllib.parse import urlparse
 
 import pyarrow as pa
 
