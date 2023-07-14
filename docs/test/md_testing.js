3e056e853a237ff38c6ced37112fe76b11f60070
TevinWang <tevin.wang@warriorlife.net>
2023-07-14 10:23:17 -0700

    exclude example

--- docs/test/md_testing.js
+++ docs/test/md_testing.js
@@ -6,7 +6,9 @@ const excludedFiles = [
   "../src/fts.md",
   "../src/embedding.md",
   "../src/examples/serverless_lancedb_with_s3_and_lambda.md",
+  "../src/examples/serverless_lancedb_with_s3_and_lambda.md",
   "../src/examples/serverless_qa_bot_with_modal_and_langchain.md",
+  "../src/examples/transformerjs_embedding_search_nodejs.md",
   "../src/examples/youtube_transcript_bot_with_nodejs.md",
 ];
 const nodePrefix = "javascript";
