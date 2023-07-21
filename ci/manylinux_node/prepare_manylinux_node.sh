35b47a1b9749f5855bb1913880140b16a6a14d84
Will Jones <willjones127@gmail.com>
2023-07-21 16:07:44 -0700

    cleanup

--- ci/manylinux_node/prepare_manylinux_node.sh
+++ ci/manylinux_node/prepare_manylinux_node.sh
@@ -1,44 +1,13 @@
 set -e
 
-# setup_dependencies() {
-#     echo "Installing system dependencies..."
-    
-#     # manylinux2014
-#     # yum update && yum install -y openssl-devel unzip
-
-#     if [[ $1 == x86_64* ]]; then
-#         ARCH=x86_64
-#     else
-#         # gnu target
-#         ARCH=aarch_64
-#     fi
-
-#     # Install new enough protobuf (yum-provided is old)
-#     PB_REL=https://github.com/protocolbuffers/protobuf/releases
-#     PB_VERSION=23.1
-#     curl -LO $PB_REL/download/v$PB_VERSION/protoc-$PB_VERSION-linux-$ARCH.zip
-#     sudo unzip protoc-$PB_VERSION-linux-$ARCH.zip -d /usr/local
-# }
-
 install_node() {
     echo "Installing node..."
-    # mkdir /usr/local/nvm
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-    source "$HOME"/.bashrc
-        # NVM_DIR=/usr/local/nvm bash
-    
-    # echo "#!/usr/bin/env bash\nexport NVM_DIR=\"/usr/local/nvm\n\"" > /etc/profile.d/nvm.sh
-    # echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"  # This loads nvm" >> /etc/profile.d/nvm.sh
 
-    # source /etc/profile.d/nvm.sh
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+    source "$HOME"/.bashrc
 
     nvm install --no-progress 16
-
-    # Make available for all users
-    # n=$(which node)
-    # n=${n%/bin/node}
-    # chmod -R 755 $n/bin/*
-    # cp -r $n/{bin,lib,share} /usr/local
 }
 
 install_rust() {
@@ -49,6 +18,5 @@ install_rust() {
 
 TARGET=${1:-x86_64} # or aarch64
 
-# setup_dependencies $TARGET
 install_node $TARGET
 install_rust
