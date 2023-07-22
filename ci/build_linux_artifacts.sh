97da683f702919c4e09b9b985d963dc5894c659e
Will Jones <willjones127@gmail.com>
2023-07-21 20:37:16 -0700

    fix: handle user id

--- ci/build_linux_artifacts.sh
+++ ci/build_linux_artifacts.sh
@@ -1,10 +1,12 @@
 ARCH=${1:-x86_64}
 
+# We pass down the current user so that when we later mount the local files 
+# into the container, the files are accessible by the current user.
 pushd ci/manylinux_node
 docker build \
     -t lancedb-node-manylinux \
     --build-arg="ARCH=$ARCH" \
-    --build-arg="DOCKER_USER=will" \
+    --build-arg="DOCKER_USER=$(id -u)" \
     --progress=plain \
     .
 popd
