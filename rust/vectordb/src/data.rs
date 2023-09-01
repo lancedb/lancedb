a2fe4fe34b46e09ec8e43f76ee052707cbbf23de
Lei Xu <lei@lancedb.com>
2023-08-31 20:52:10 -0700

    casting

--- rust/vectordb/src/data.rs
+++ rust/vectordb/src/data.rs
@@ -16,10 +16,51 @@
 
 use std::sync::Arc;
 
-use arrow_array::{RecordBatch, RecordBatchIterator, RecordBatchReader};
-use arrow_schema::{ArrowError, Schema};
+use arrow_array::{Array, RecordBatch, RecordBatchIterator, RecordBatchReader, FixedSizeListArray, cast::AsArray};
+use arrow_schema::{ArrowError, DataType, Schema, Field};
+use arrow_cast::{cast, can_cast_types};
+use lance::arrow::{DataTypeExt, FixedSizeListArrayExt};
+use log::warn;
 
 use crate::error::Result;
+
+fn coerce_array(
+    array: &Arc<dyn Array>,
+    field: &Field,
+) -> std::result::Result<Arc<dyn Array>, ArrowError> {
+    if array.data_type() == field.data_type() {
+        return Ok(array.clone());
+    }
+    match (array.data_type(), field.data_type()) {
+        (adt, dt) if (adt.is_floating() || dt.is_floating()) => {
+            if adt.byte_width() < dt.byte_width() {
+                warn!("Coercing field {} {:?} to {:?} might lose precision", field.name(), adt, dt);
+            }
+            cast(&array, dt)
+        },
+        (adt, DataType::FixedSizeList(exp_field, exp_dim)) => {
+            match adt {
+                DataType::FixedSizeList(sub_field, dim) if dim == exp_dim => {
+                    let actual_sub = array.as_fixed_size_list();
+                    let values = coerce_array(&actual_sub.values(), exp_field)?;
+                    Ok(Arc::new(FixedSizeListArray::try_new_from_values(values.clone(), *dim)?) as Arc<dyn Array>)
+                },
+                _ => Err(ArrowError::SchemaError(format!(
+                    "Incompatible coerce fixed size list field {}: unable to coerce {:?} to {:?}",
+                    field.name(),
+                    array.data_type(),
+                    field.data_type()
+                )))?,
+            }
+        }
+        _ => Err(ArrowError::SchemaError(format!(
+            "Incompatible change field {}: unable to coerce {:?} to {:?}",
+            field.name(),
+            array.data_type(),
+            field.data_type()
+        )))?,
+    }
+}
 
 fn coerce_schema_batch(
     batch: RecordBatch,
@@ -34,13 +75,7 @@ fn coerce_schema_batch(
         .map(|field| {
             batch
                 .column_by_name(field.name())
-                .map(|c| {
-                    if c.data_type() == field.data_type() {
-                        return Ok(c.clone());
-                    } else {
-                        todo!()
-                    }
-                })
+                .map(|c| coerce_array(c, field))
                 .ok_or(|| {
                     ArrowError::SchemaError(format!("Column {} not found in batch", field.name()))
                 })
@@ -75,9 +110,9 @@ mod tests {
     use std::sync::Arc;
 
     use arrow_array::{
-        FixedSizeListArray, Float16Array, RecordBatch, RecordBatchIterator, StringArray,
+        FixedSizeListArray, Float32Array, RecordBatch, RecordBatchIterator, StringArray,
     };
-    use arrow_schema::{DataType, Field};
+    use arrow_schema::Field;
     use half::f16;
     use lance::arrow::FixedSizeListArrayExt;
 
@@ -86,7 +121,7 @@ mod tests {
         let schema = Arc::new(Schema::new(vec![
             Field::new(
                 "fl",
-                DataType::FixedSizeList(Arc::new(Field::new("item", DataType::Float16, true)), 64),
+                DataType::FixedSizeList(Arc::new(Field::new("item", DataType::Float32, true)), 64),
                 true,
             ),
             Field::new("s", DataType::Utf8, true),
@@ -97,7 +132,7 @@ mod tests {
             vec![
                 Arc::new(
                     FixedSizeListArray::try_new_from_values(
-                        Float16Array::from_iter_values((0..256).map(|v| f16::from_f32(v as f32))),
+                        Float32Array::from_iter_values((0..256).map(|v| v as f32)),
                         64,
                     )
                     .unwrap(),
@@ -116,11 +151,15 @@ mod tests {
         let expected_schema = Arc::new(Schema::new(vec![
             Field::new(
                 "fl",
-                DataType::FixedSizeList(Arc::new(Field::new("item", DataType::Float32, true)), 64),
+                DataType::FixedSizeList(Arc::new(Field::new("item", DataType::Float16, true)), 64),
                 true,
             ),
             Field::new("s", DataType::Utf8, true),
         ]));
-        let stream = coerce_schema(reader, expected_schema).unwrap();
+        let stream = coerce_schema(reader, expected_schema.clone()).unwrap();
+        let batches = stream.collect::<Vec<_>>();
+        assert_eq!(batches.len(), 1);
+        let batch = batches[0].as_ref().unwrap();
+        assert_eq!(batch.schema(), expected_schema);
     }
 }
