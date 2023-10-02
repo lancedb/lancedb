9fa8935c920e42ab1a1189842d98ba80de4ba5c0
Will Jones <willjones127@gmail.com>
2023-10-02 09:04:02 -0700

    small issues

--- python/tests/test_table.py
+++ python/tests/test_table.py
@@ -11,8 +11,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from datetime import timedelta
 import functools
+from datetime import timedelta
 from pathlib import Path
 from typing import List
 from unittest.mock import PropertyMock, patch
