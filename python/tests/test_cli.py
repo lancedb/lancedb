1a7ddb9cdec8890212f8eb7e6ad8dfdd0f3aefda
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2023-10-06 13:10:15 +0530

    add loose tests

--- python/tests/test_cli.py
+++ python/tests/test_cli.py
@@ -2,27 +2,33 @@ from lancedb.cli.cli import cli
 from lancedb.utils import CONFIG
 from click.testing import CliRunner
 
+
 def test_entry():
-  runner = CliRunner()
-  result = runner.invoke(cli)
-  assert result.exit_code == 0 # Main check
-  assert "lancedb" in result.output.lower() # lazy check
+    runner = CliRunner()
+    result = runner.invoke(cli)
+    assert result.exit_code == 0  # Main check
+    assert "lancedb" in result.output.lower()  # lazy check
+
 
 def test_diagnostics():
-  runner = CliRunner()
-  result = runner.invoke(cli, ["diagnostics", "--disabled"])
-  assert result.exit_code == 0 # Main check
-  assert CONFIG["diagnostics"] == False
+    runner = CliRunner()
+    result = runner.invoke(cli, ["diagnostics", "--disabled"])
+    assert result.exit_code == 0  # Main check
+    assert CONFIG["diagnostics"] == False
 
-  result = runner.invoke(cli, ["diagnostics", "--enabled"])
-  assert result.exit_code == 0 # Main check
-  assert CONFIG["diagnostics"] == True
+    result = runner.invoke(cli, ["diagnostics", "--enabled"])
+    assert result.exit_code == 0  # Main check
+    assert CONFIG["diagnostics"] == True
+
 
 def test_config():
     runner = CliRunner()
     result = runner.invoke(cli, ["config"])
-    assert result.exit_code == 0 # Main check
+    assert result.exit_code == 0  # Main check
     cfg = CONFIG.copy()
     cfg.pop("uuid")
-    for item, _ in cfg.items(): # check for keys only as formatting is subject to change
+    for (
+        item,
+        _,
+    ) in cfg.items():  # check for keys only as formatting is subject to change
         assert item in result.output
---
+++ python/tests/test_telemetry.py
@@ -0,0 +1,45 @@
+import pytest
+import requests
+import lancedb
+from lancedb.utils.events import _Events
+from lancedb.utils.general import threaded_request
+
+
+def mock_request(
+    method, url, retry=3, timeout=30, thread=True, code=-1, verbose=True, **kwargs
+):
+    assert method.lower() == "post"
+    json = kwargs.get("json", {})
+    # TODO: don't hardcode these here. Instead create a module level json scehma in lancedb.utils.events for better evolvability
+    batch_keys = ["api_key", "distinct_id", "batch"]
+    event_keys = ["event", "properties", "timestamp", "distinct_id"]
+    property_keys = ["cli", "install", "platforms", "version", "session_id", "blud"]
+
+    assert all([key in json for key in batch_keys])
+    assert all([key in json["batch"][0] for key in event_keys])
+    assert all([key in json["batch"][0]["properties"] for key in property_keys])
+
+
+def mock_register_event(name: str, **kwargs):
+    if _Events._instance is None:
+        _Events._instance = _Events()
+        _Events._instance.enabled = True
+        _Events._instance.rate_limit = 0
+
+    _Events._instance(name, **kwargs)
+
+
+# notice our test uses the custom fixture instead of monkeypatch directly
+def test_get_access_token_success(monkeypatch) -> None:
+    monkeypatch.setattr(lancedb.table, "register_event", mock_register_event)
+    monkeypatch.setattr(lancedb.utils.events, "threaded_request", mock_request)
+
+    db = lancedb.connect("db")
+    db.create_table(
+        "test",
+        data=[
+            {"vector": [3.1, 4.1], "item": "foo", "price": 10.0},
+            {"vector": [5.9, 26.5], "item": "bar", "price": 20.0},
+        ],
+        mode="overwrite",
+    )
