bff3c0bfe173017e54c263a3cb4209ca411631a0
Rok Mihevc <rok@mihevc.org>
2023-10-11 20:42:08 +0200

    Review feedback

--- python/lancedb/table.py
+++ python/lancedb/table.py
@@ -292,9 +292,9 @@ class Table(ABC):
         --------
         >>> import lancedb
         >>> import pandas as pd
-        >>> data = pd.DataFrame([
+        >>> data = [
         ...   {"x": 1, "vector": [1, 2]}, {"x": 2, "vector": [3, 4]}, {"x": 3, "vector": [5, 6]}
-        ... ])
+        ... ]
         >>> db = lancedb.connect("./.lancedb")
         >>> table = db.create_table("my_table", data)
         >>> table.to_pandas()
@@ -710,9 +710,9 @@ class LanceTable(Table):
         --------
         >>> import lancedb
         >>> import pandas as pd
-        >>> data = pd.DataFrame([
+        >>> data = [
         ...   {"x": 1, "vector": [1, 2]}, {"x": 2, "vector": [3, 4]}, {"x": 3, "vector": [5, 6]}
-        ... ])
+        ... ]
         >>> db = lancedb.connect("./.lancedb")
         >>> table = db.create_table("my_table", data)
         >>> table.to_pandas()
