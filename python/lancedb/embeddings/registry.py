f1c795903394284c89d51217eef07fd19107fd1f
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2023-10-17 21:55:06 +0530

    doctests

--- python/lancedb/embeddings/registry.py
+++ python/lancedb/embeddings/registry.py
@@ -11,7 +11,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import json
-from typing import Dict, Optional
+from typing import Dict, Optional, List
 
 from .base import EmbeddingFunction, EmbeddingFunctionConfig
 
