bda0135cfcec27a1d8f6fed97e4f13ae550d2042
QianZhu <qian@lancedb.com>
2023-12-07 14:47:56 -0800

    saas python sdk doc (#692)
    <img width="256" alt="Screenshot 2023-12-07 at 11 55 41 AM"
    src="https://github.com/lancedb/lancedb/assets/1305083/259bf234-9b3b-4c5d-af45-c7f3fada2cc7">

--- python/lancedb/__init__.py
+++ python/lancedb/__init__.py
@@ -26,7 +26,7 @@ def connect(
     uri: URI,
     *,
     api_key: Optional[str] = None,
-    region: str = "us-west-2",
+    region: str = "us-east-1",
     host_override: Optional[str] = None,
 ) -> DBConnection:
     """Connect to a LanceDB database.
@@ -38,7 +38,7 @@ def connect(
     api_key: str, optional
         If presented, connect to LanceDB cloud.
         Otherwise, connect to a database on file system or cloud storage.
-    region: str, default "us-west-2"
+    region: str, default "us-east-1"
         The region to use for LanceDB Cloud.
     host_override: str, optional
         The override url for LanceDB Cloud.
