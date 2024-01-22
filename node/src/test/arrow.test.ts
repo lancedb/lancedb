d77219d2cf575b311fc6bab2350fabacba5d58ec
Lei Xu <lei@lancedb.com>
2024-01-21 21:13:00 -0800

    add tesets

--- node/src/test/arrow.test.ts
+++ node/src/test/arrow.test.ts
@@ -23,11 +23,12 @@ import {
   Float32,
   Int32,
   tableFromIPC,
-  Schema
+  Schema,
+  Float64
 } from 'apache-arrow'
 
 describe('Apache Arrow tables', function () {
-  it('Customized schema', async function () {
+  it('customized schema', async function () {
     const schema = new Schema([
       new Field('a', new Int32()),
       new Field('b', new Float32()),
@@ -42,7 +43,55 @@ describe('Apache Arrow tables', function () {
       { schema }
     )
 
-    console.log(table.schema.toString())
+    const buf = await fromTableToBuffer(table)
+    assert.isAbove(buf.byteLength, 0)
+
+    const actual = tableFromIPC(buf)
+    assert.equal(actual.numRows, 3)
+    const actualSchema = actual.schema
+    assert.deepEqual(actualSchema, schema)
+  })
+
+  it('default vector column', async function () {
+    const schema = new Schema([
+      new Field('a', new Float64()),
+      new Field('b', new Float64()),
+      new Field(
+        'vector',
+        new FixedSizeList(3, new Field('item', new Float32()))
+      )
+    ])
+    const table = makeArrowTable([
+      { a: 1, b: 2, vector: [1, 2, 3] },
+      { a: 4, b: 5, vector: [4, 5, 6] },
+      { a: 7, b: 8, vector: [7, 8, 9] }
+    ])
+
+    const buf = await fromTableToBuffer(table)
+    assert.isAbove(buf.byteLength, 0)
+
+    const actual = tableFromIPC(buf)
+    assert.equal(actual.numRows, 3)
+    const actualSchema = actual.schema
+    assert.deepEqual(actualSchema, schema)
+  })
+
+  it('2 vector columns', async function () {
+    const schema = new Schema([
+      new Field('a', new Float64()),
+      new Field('b', new Float64()),
+      new Field('vec1', new FixedSizeList(3, new Field('item', new Float16()))),
+      new Field('vec2', new FixedSizeList(3, new Field('item', new Float16())))
+    ])
+    const table = makeArrowTable(
+      [
+        { a: 1, b: 2, vec1: [1, 2, 3], vec2: [2, 4, 6] },
+        { a: 4, b: 5, vec1: [4, 5, 6], vec2: [8, 10, 12] },
+        { a: 7, b: 8, vec1: [7, 8, 9], vec2: [14, 16, 18] }
+      ],
+      { vectorColumns: ['vec1', 'vec2'], vectorDataType: new Float16() }
+    )
+
     const buf = await fromTableToBuffer(table)
     assert.isAbove(buf.byteLength, 0)
 
