00dec726a8b85c8dda03cd8d9681f1e7eb9a62f4
Lei Xu <eddyxu@gmail.com>
2024-01-23 10:58:39 -0800

    evn longer

--- node/src/test/test.ts
+++ node/src/test/test.ts
@@ -389,7 +389,7 @@ describe('LanceDB client', function () {
           'vector column is list of floats'
         )
       })
-    }).timeout(20000)
+    }).timeout(60000)
 
     it('fails to create a new table when the vector column is missing', async function () {
       const dir = await track().mkdir('lancejs')
