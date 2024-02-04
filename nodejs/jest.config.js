ebb54812e853aeb58610959edac6ebfd37e91a8c
Lei Xu <eddyxu@gmail.com>
2024-02-04 14:01:52 -0800

    ignore some tests

--- nodejs/jest.config.js
+++ nodejs/jest.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", ".*vectordb-nodejs.*\.node\b"],
+  modulePathIgnorePatterns: [".*\.node"]
 };
