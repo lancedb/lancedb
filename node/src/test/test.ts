fb7ad0feb0d24955d27fe508fd06e22c201f094e
Weston Pace <weston.pace@gmail.com>
2024-02-07 15:00:26 -0800

    Remove debug logging from test

--- node/src/test/test.ts
+++ node/src/test/test.ts
@@ -838,8 +838,6 @@ describe('LanceDB client', function () {
       const names = vectorFromArray(['foo', 'bar'], new Utf8())
       const data = new ArrowTable({ name: names })
 
-      console.log(data.schema.fields)
-
       const table = await con.createTable({
         name: 'vectors',
         data,
