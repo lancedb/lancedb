9c4d4cb5284dc6b2978f5734314ca387624fb338
Chang She <759245+changhiskhan@users.noreply.github.com>
2024-02-16 22:02:06 -0800

    ruff format .

--- python/lancedb/util.py
+++ python/lancedb/util.py
@@ -247,13 +247,19 @@ def deprecated(func):
     """This is a decorator which can be used to mark functions
     as deprecated. It will result in a warning being emitted
     when the function is used."""
+
     @functools.wraps(func)
     def new_func(*args, **kwargs):
-        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
-        warnings.warn((f"Function {func.__name__} is deprecated and will be "
-                       "removed in a future version"),
-                      category=DeprecationWarning,
-                      stacklevel=2)
-        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        warnings.simplefilter("always", DeprecationWarning)  # turn off filter
+        warnings.warn(
+            (
+                f"Function {func.__name__} is deprecated and will be "
+                "removed in a future version"
+            ),
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
+        warnings.simplefilter("default", DeprecationWarning)  # reset filter
         return func(*args, **kwargs)
+
     return new_func
