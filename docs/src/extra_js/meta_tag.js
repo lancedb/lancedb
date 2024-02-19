b5aad2d856b688976c6cedbfc92f4244e86b6eff
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2024-02-19 14:07:31 +0530

    docs: Add meta tag for image preview (#988)
    I think this should work. Need to deploy it to be sure as it can be
    tested locally. Can be tested here.
    
    2 things about this solution:
    * All pages have a same meta tag, i.e, lancedb banner
    * If needed, we can automatically use the first image of each page and
    generate meta tags using the ultralytics mkdocs plugin that we did for
    this purpose - https://github.com/ultralytics/mkdocs

---
+++ docs/src/extra_js/meta_tag.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    var meta = document.createElement('meta');
+    meta.setAttribute('property', 'og:image');
+    meta.setAttribute('content', '/assets/lancedb_and_lance.png');
+    document.head.appendChild(meta);
+  });
