538d0320f7af60064eae725d62d20fe36a894f41
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2024-02-21 23:22:47 +0530

    Docs: add meta tags (#1006)

--- docs/src/extra_js/meta_tag.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    var meta = document.createElement('meta');
-    meta.setAttribute('property', 'og:image');
-    meta.setAttribute('content', '/assets/lancedb_and_lance.png');
-    document.head.appendChild(meta);
-  });
