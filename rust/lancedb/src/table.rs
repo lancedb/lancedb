89cad042bd8486e80e2aef0297a9063e1a526203
Will Jones <willjones127@gmail.com>
2024-03-06 15:49:11 -0800

    fix table

--- rust/lancedb/src/table.rs
+++ rust/lancedb/src/table.rs
@@ -504,6 +504,13 @@ impl Table {
     }
 }
 
+impl From<NativeTable> for Table {
+    fn from(table: NativeTable) -> Self {
+        Self {
+            inner: Arc::new(table),
+        }
+    }
+}
 /// A table in a LanceDB database.
 #[derive(Debug, Clone)]
 pub struct NativeTable {
