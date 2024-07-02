46c6ff889d37733c277f3b40d95c317af30f1c8b
Nuvic <78891638+nuvic@users.noreply.github.com>
2024-07-02 11:10:01 -0700

    feat: add the explain_plan function (#1328)
    It's useful to see the underlying query plan for debugging purposes.
    This exposes LanceScanner's `explain_plan` function. Addresses #1288
    
    ---------
    
    Co-authored-by: Will Jones <willjones127@gmail.com>

--- python/python/tests/test_query.py
+++ python/python/tests/test_query.py
@@ -333,3 +333,15 @@ async def test_query_to_pandas_async(table_async: AsyncTable):
 
     df = await table_async.query().where("id < 0").to_pandas()
     assert df.shape == (0, 4)
+
+
+def test_explain_plan(table):
+    q = LanceVectorQueryBuilder(table, [0, 0], "vector")
+    plan = q.explain_plan(verbose=True)
+    assert "KNN" in plan
+
+
+@pytest.mark.asyncio
+async def test_explain_plan_async(table_async: AsyncTable):
+    plan = await table_async.query().nearest_to(pa.array([1, 2])).explain_plan(True)
+    assert "KNN" in plan
