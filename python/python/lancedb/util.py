b5210de8dd955ee71013fd98b98abbe44c07dfb8
Lei Xu <eddyxu@gmail.com>
2024-07-10 11:12:44 -0700

    add binary conversion

--- python/python/lancedb/util.py
+++ python/python/lancedb/util.py
@@ -1,16 +1,7 @@
-#  Copyright 2023 LanceDB Developers
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright The Lance Authors
 
+import binascii
 import functools
 import importlib
 import os
@@ -229,6 +220,15 @@ def value_to_sql(value):
 @value_to_sql.register(str)
 def _(value: str):
     return f"'{value}'"
+
+
+@value_to_sql.register(bytes)
+def _(value: bytes):
+    """Convert bytes to a hex string literal.
+
+    See https://datafusion.apache.org/user-guide/sql/data_types.html#binary-types
+    """
+    return f"X'{binascii.hexlify(value).decode()}'"
 
 
 @value_to_sql.register(int)
