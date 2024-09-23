f5c25b6fff5ab97b1016239034605256bfcd54a4
Will Jones <willjones127@gmail.com>
2024-09-23 07:33:47 -0700

    ci: run clippy on tests (#1659)

--- rust/lancedb/src/table.rs
+++ rust/lancedb/src/table.rs
@@ -3234,7 +3234,7 @@ mod tests {
         let values_builder = StringBuilder::new();
         let mut builder = ListBuilder::new(values_builder);
         for i in 0..120 {
-            builder.values().append_value(TAGS[i % 3].to_string());
+            builder.values().append_value(TAGS[i % 3]);
             if i % 3 == 0 {
                 builder.append(true)
             }
