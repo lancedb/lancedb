026a9c41810a78ee705612b349ee4fe3f9756a4b
Will Jones <willjones127@gmail.com>
2024-10-08 19:45:20 -0600

    feat: upgrade Lance to v0.18.2

--- node/src/test/test.ts
+++ node/src/test/test.ts
@@ -116,6 +116,10 @@ describe("LanceDB client", function () {
         }
       ]);
       assert.equal(await table2.countRows(), 3);
+
+      await table1.delete("id <> 3")
+
+      assert.equal(await table2.countRows(), 1);
     });
   });
 
