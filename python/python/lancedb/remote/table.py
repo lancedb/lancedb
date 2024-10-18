b91e76ec485bc28031bdec9901fbc3d8ee883117
Will Jones <willjones127@gmail.com>
2024-10-18 15:51:09 -0700

    don't cache schema

--- python/python/lancedb/remote/table.py
+++ python/python/lancedb/remote/table.py
@@ -49,7 +49,7 @@ class RemoteTable(Table):
     def __len__(self) -> int:
         self.count_rows(None)
 
-    @cached_property
+    @property
     def schema(self) -> pa.Schema:
         """The [Arrow Schema](https://arrow.apache.org/docs/python/api/datatypes.html#)
         of this Table
