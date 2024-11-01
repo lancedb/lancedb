1c78766aaf539521fe11831ac6c3337c812a90d2
fzowl <zoltan@voyageai.com>
2024-11-01 12:37:23 +0100

    Adding voyageai support

--- python/python/tests/docs/test_embeddings_optional.py
+++ python/python/tests/docs/test_embeddings_optional.py
@@ -8,20 +8,20 @@ from lancedb.embeddings import get_registry
 import pytest
 
 
-# @pytest.mark.slow
-# def test_embeddings_openai():
-#     # --8<-- [start:openai_embeddings]
-#     db = lancedb.connect("/tmp/db")
-#     func = get_registry().get("openai").create(name="text-embedding-ada-002")
-#
-#     class Words(LanceModel):
-#         text: str = func.SourceField()
-#         vector: Vector(func.ndims()) = func.VectorField()
-#
-#     table = db.create_table("words", schema=Words, mode="overwrite")
-#     table.add([{"text": "hello world"}, {"text": "goodbye world"}])
-#
-#     query = "greetings"
-#     actual = table.search(query).limit(1).to_pydantic(Words)[0]
-#     print(actual.text)
-#     # --8<-- [end:openai_embeddings]
+@pytest.mark.slow
+def test_embeddings_openai():
+    # --8<-- [start:openai_embeddings]
+    db = lancedb.connect("/tmp/db")
+    func = get_registry().get("openai").create(name="text-embedding-ada-002")
+
+    class Words(LanceModel):
+        text: str = func.SourceField()
+        vector: Vector(func.ndims()) = func.VectorField()
+
+    table = db.create_table("words", schema=Words, mode="overwrite")
+    table.add([{"text": "hello world"}, {"text": "goodbye world"}])
+
+    query = "greetings"
+    actual = table.search(query).limit(1).to_pydantic(Words)[0]
+    print(actual.text)
+    # --8<-- [end:openai_embeddings]
