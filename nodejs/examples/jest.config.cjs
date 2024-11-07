845015ba08d1b71e0811b6f1391d2798023b2d17
Will Jones <willjones127@gmail.com>
2024-11-07 11:54:20 -0800

    lint fixes

--- nodejs/examples/jest.config.cjs
+++ nodejs/examples/jest.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    testPathIgnorePatterns: ["./dist"],
-  };
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["./dist"],
+};
