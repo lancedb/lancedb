3b769d7e3c2099eedd04843f85cd8e82ab74eb2a
Will Jones <willjones127@gmail.com>
2024-12-13 11:48:36 -0800

    fix for python 3.9

--- python/python/tests/utils.py
+++ python/python/tests/utils.py
@@ -7,6 +7,5 @@ def exception_output(e_info: pytest.ExceptionInfo):
     import traceback
 
     # skip traceback part, since it's not worth checking in tests
-    e_info.value.__traceback__ = None
-    lines = traceback.format_exception(e_info.value)
+    lines = traceback.format_exception_only(type(e_info), e_info.value)
     return "".join(lines).strip()
