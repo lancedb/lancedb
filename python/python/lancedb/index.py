90a4099d64d4c3c97e7868dce90d06d3144edda7
Renato Marroquin <renato.marroquin@oracle.com>
2024-12-16 23:31:21 +0100

    1855: Add new indexes to python docs

--- python/python/lancedb/index.py
+++ python/python/lancedb/index.py
@@ -477,4 +477,4 @@ class IvfPq:
     sample_rate: int = 256
 
 
-__all__ = ["BTree", "IvfPq", "HnswPq", "HnswSq", "IndexConfig"]
+__all__ = ["BTree", "IvfPq", "HnswPq", "HnswSq", "IndexConfig", "FTS", "Bitmap", "LabelList"]
