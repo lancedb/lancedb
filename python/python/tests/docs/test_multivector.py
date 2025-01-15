668e8b7905d5e0b30b2e6c2ccac5918354e89d1a
BubbleCal <bubble-cal@outlook.com>
2025-01-15 12:34:43 +0800

    fix
    Signed-off-by: BubbleCal <bubble-cal@outlook.com>

--- python/python/tests/docs/test_multivector.py
+++ python/python/tests/docs/test_multivector.py
@@ -4,22 +4,29 @@ import pytest
 # --8<-- [start:imports]
 import lancedb
 import numpy as np
+import pyarrow as pa
 # --8<-- [end:imports]
 
 shutil.rmtree("data/multivector_demo", ignore_errors=True)
 
 
-def test_binary_vector():
+def test_multivector():
     # --8<-- [start:sync_multivector]
     db = lancedb.connect("data/multivector_demo")
+    schema = pa.schema(
+        [
+            pa.field("id", pa.int64()),
+            pa.field("vector", pa.list_(pa.list_(pa.float32(), 256))),
+        ]
+    )
     data = [
         {
             "id": i,
-            "vector": np.random.random(size=(2, 256)),
+            "vector": np.random.random(size=(2, 256)).tolist(),
         }
         for i in range(1024)
     ]
-    tbl = db.create_table("my_table", data=data)
+    tbl = db.create_table("my_table", data=data, schema=schema)
 
     # query with single vector
     query = np.random.random(256)
@@ -34,17 +41,23 @@ def test_binary_vector():
 
 
 @pytest.mark.asyncio
-async def test_binary_vector_async():
+async def test_multivector_async():
     # --8<-- [start:async_multivector]
     db = await lancedb.connect_async("data/multivector_demo")
+    schema = pa.schema(
+        [
+            pa.field("id", pa.int64()),
+            pa.field("vector", pa.list_(pa.list_(pa.float32(), 256))),
+        ]
+    )
     data = [
         {
             "id": i,
-            "vector": np.random.random(size=(2, 256)),
+            "vector": np.random.random(size=(2, 256)).tolist(),
         }
         for i in range(1024)
     ]
-    tbl = await db.create_table("my_table", data=data)
+    tbl = await db.create_table("my_table", data=data, schema=schema)
 
     # query with single vector
     query = np.random.random(256)
