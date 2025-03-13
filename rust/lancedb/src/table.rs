5c00b2904ce0cf8e3eb6d9895b1fdd34b8e6f616
Bob Liu <akagi201@gmail.com>
2025-03-14 02:15:28 +0800

    feat: add get dataset method on NativeTable (#2021)
    I want to public the dataset method from native table, then I can use
    more lance method like order_by which is not exposed in the lancedb
    crate.

--- rust/lancedb/src/table.rs
+++ rust/lancedb/src/table.rs
@@ -410,6 +410,8 @@ pub trait BaseTable: std::fmt::Display + std::fmt::Debug + Send + Sync {
     fn as_any(&self) -> &dyn std::any::Any;
     /// Get the name of the table.
     fn name(&self) -> &str;
+    /// Get the dataset of the table.
+    fn dataset(&self) -> &dataset::DatasetConsistencyWrapper;
     /// Get the arrow [Schema] of the table.
     async fn schema(&self) -> Result<SchemaRef>;
     /// Count the number of rows in this table.
@@ -579,6 +581,11 @@ impl Table {
     /// Get the name of the table.
     pub fn name(&self) -> &str {
         self.inner.name()
+    }
+
+    /// Get the dataset of the table.
+    pub fn dataset(&self) -> &dataset::DatasetConsistencyWrapper {
+        self.inner.dataset()
     }
 
     /// Get the arrow [Schema] of the table.
@@ -1856,6 +1863,10 @@ impl BaseTable for NativeTable {
 
     fn name(&self) -> &str {
         self.name.as_str()
+    }
+
+    fn dataset(&self) -> &dataset::DatasetConsistencyWrapper {
+        &self.dataset
     }
 
     async fn version(&self) -> Result<u64> {
