a59aa2ebc3fb77c88e8c9a370bed8dfb137875e1
Will Jones <willjones127@gmail.com>
2025-03-19 15:30:09 -0700

    more simplification

--- ci/manylinux_node/build_vectordb.sh
+++ ci/manylinux_node/build_vectordb.sh
@@ -4,14 +4,6 @@ set -e
 ARCH=${1:-x86_64}
 TARGET_TRIPLE=${2:-x86_64-unknown-linux-gnu}
 
-if [ "$ARCH" = "x86_64" ]; then
-    export OPENSSL_LIB_DIR=/usr/local/lib64/
-else
-    export OPENSSL_LIB_DIR=/usr/local/lib/
-fi
-export OPENSSL_STATIC=1
-export OPENSSL_INCLUDE_DIR=/usr/local/include/openssl
-
 #Alpine doesn't have .bashrc
 FILE=$HOME/.bashrc && test -f $FILE && source $FILE
 
