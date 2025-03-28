8d12030c415b1b207ee04f63f983ed175586b638
fzowl <zoltan@voyageai.com>
2025-03-28 18:11:13 +0100

    Correcting the issue

--- python/python/lancedb/embeddings/voyageai.py
+++ python/python/lancedb/embeddings/voyageai.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright The LanceDB Authors
-
-
+import base64
 import os
 from typing import ClassVar, TYPE_CHECKING, List, Union, Any
 
@@ -11,7 +10,6 @@ from io import BytesIO
 
 import numpy as np
 import pyarrow as pa
-import requests
 
 from ..util import attempt_import_or_raise
 from .base import EmbeddingFunction
@@ -30,30 +28,46 @@ def is_valid_url(text):
         return False
 
 
-def transform_input(input: Union[str, bytes, Path]):
+def transform_input(input_data: Union[str, bytes, Path]):
     PIL = attempt_import_or_raise("PIL", "pillow")
-    if isinstance(input, str):
-        if is_valid_url(input):
-            try:
-                response = requests.get(input)
-                return PIL.Image.open(BytesIO(response.content))
-            except Exception as e:
-                return input
+    if isinstance(input_data, str):
+        if is_valid_url(input_data):
+            content = {"type": "image_url", "image_url": input_data}
         else:
-            return input
-    elif isinstance(input, PIL.Image.Image):
-        return input
-    elif isinstance(input, bytes):
-        return PIL.Image.open(BytesIO(input))
-    elif isinstance(input, Path):
-        return PIL.Image.open(input)
+            content = {"type": "text", "text": input_data}
+    elif isinstance(input_data, PIL.Image.Image):
+        buffered = BytesIO()
+        input_data.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        content = {
+            "type": "image_base64",
+            "image_base64": "data:image/jpeg;base64," + img_str,
+        }
+    elif isinstance(input_data, bytes):
+        img = PIL.Image.open(BytesIO(input_data))
+        buffered = BytesIO()
+        img.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        content = {
+            "type": "image_base64",
+            "image_base64": "data:image/jpeg;base64," + img_str,
+        }
+    elif isinstance(input_data, Path):
+        img = PIL.Image.open(input_data)
+        buffered = BytesIO()
+        img.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        content = {
+            "type": "image_base64",
+            "image_base64": "data:image/jpeg;base64," + img_str,
+        }
     else:
-        raise ValueError(f"Each input should be either str, bytes, Path or Image.")
+        raise ValueError("Each input should be either str, bytes, Path or Image.")
+
+    return {"content": [content]}
 
 
-def sanitize_multmodal_input(
-        inputs: Union[TEXT, IMAGES]
-) -> List[Any]:
+def sanitize_multimodal_input(inputs: Union[TEXT, IMAGES]) -> List[Any]:
     """
     Sanitize the input to the embedding function.
     """
@@ -65,17 +79,17 @@ def sanitize_multmodal_input(
     elif isinstance(inputs, pa.ChunkedArray):
         inputs = inputs.combine_chunks().to_pylist()
     else:
-        raise ValueError(f"Input type {type(inputs)} not allowed with multimodal model.")
+        raise ValueError(
+            f"Input type {type(inputs)} not allowed with multimodal model."
+        )
 
     if not all(isinstance(x, (str, bytes, Path, PIL.Image.Image)) for x in inputs):
-        raise ValueError(f"Each input should be either str, bytes, Path or Image.")
+        raise ValueError("Each input should be either str, bytes, Path or Image.")
 
-    return [[transform_input(i)] for i in inputs]
+    return [transform_input(i) for i in inputs]
 
 
-def sanitize_text_input(
-        inputs: TEXT
-) -> List[Any]:
+def sanitize_text_input(inputs: TEXT) -> List[str]:
     """
     Sanitize the input to the embedding function.
     """
@@ -89,7 +103,7 @@ def sanitize_text_input(
         raise ValueError(f"Input type {type(inputs)} not allowed with text model.")
 
     if not all(isinstance(x, str) for x in inputs):
-        raise ValueError(f"Each input should be str.")
+        raise ValueError("Each input should be str.")
 
     return inputs
 
@@ -152,7 +166,9 @@ class VoyageAIEmbeddingFunction(EmbeddingFunction):
     multimodal_embedding_models: list = ["voyage-multimodal-3"]
 
     def _is_multimodal_model(self, model_name: str):
-        return model_name in self.multimodal_embedding_models or 'multmodal' in model_name
+        return (
+            model_name in self.multimodal_embedding_models or "multimodal" in model_name
+        )
 
     def ndims(self):
         if self.name == "voyage-3-lite":
@@ -172,7 +188,7 @@ class VoyageAIEmbeddingFunction(EmbeddingFunction):
             raise ValueError(f"Model {self.name} not supported")
 
     def compute_query_embeddings(
-            self, query: Union[str, "PIL.Image.Image"], *args, **kwargs
+        self, query: Union[str, "PIL.Image.Image"], *args, **kwargs
     ) -> List[np.ndarray]:
         """
         Compute the embeddings for a given user query
@@ -181,52 +197,48 @@ class VoyageAIEmbeddingFunction(EmbeddingFunction):
         ----------
         query : Union[str, PIL.Image.Image]
             The query to embed. A query can be either text or an image.
+
+        Returns
+        -------
+            List[np.array]: the list of embeddings
         """
         client = VoyageAIEmbeddingFunction._get_client()
         if self._is_multimodal_model(self.name):
             result = client.multimodal_embed(
-                inputs=[[query]],
-                model=self.name,
-                input_type="query",
-                **kwargs
+                inputs=[[query]], model=self.name, input_type="query", **kwargs
             )
         else:
             result = client.embed(
-                texts=[query],
-                model=self.name,
-                input_type="query",
-                **kwargs
+                texts=[query], model=self.name, input_type="query", **kwargs
             )
 
         return [result.embeddings[0]]
 
     def compute_source_embeddings(
-            self, inputs: Union[TEXT, IMAGES], *args, **kwargs
+        self, inputs: Union[TEXT, IMAGES], *args, **kwargs
     ) -> List[np.array]:
         """
-        Compute the embeddings for a given user query
+        Compute the embeddings for the inputs
 
         Parameters
         ----------
         inputs : Union[TEXT, IMAGES]
-            The inputs to embed. The input can be either a str or list[str] or .
+            The inputs to embed. The input can be either str, bytes, Path (to an image), PIL.Image or list of these.
+
+        Returns
+        -------
+            List[np.array]: the list of embeddings
         """
         client = VoyageAIEmbeddingFunction._get_client()
         if self._is_multimodal_model(self.name):
-            inputs = sanitize_multmodal_input(inputs)
+            inputs = sanitize_multimodal_input(inputs)
             result = client.multimodal_embed(
-                inputs=inputs,
-                model=self.name,
-                input_type="document",
-                **kwargs
+                inputs=inputs, model=self.name, input_type="document", **kwargs
             )
         else:
             inputs = sanitize_text_input(inputs)
             result = client.embed(
-                texts=inputs,
-                model=self.name,
-                input_type="document",
-                **kwargs
+                texts=inputs, model=self.name, input_type="document", **kwargs
             )
 
         return result.embeddings
