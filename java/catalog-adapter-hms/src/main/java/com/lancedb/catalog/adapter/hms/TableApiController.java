6c7b9d6e36d5eb3f97d4b90d8a3eadec2eb72fe8
yanghua <yanghua1127@gmail.com>
2025-03-31 15:57:15 +0800

    add hms module

---
+++ java/catalog-adapter-hms/src/main/java/com/lancedb/catalog/adapter/hms/TableApiController.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.lancedb.catalog.adapter.hms;
+
+import com.lancedb.catalog.adapter.api.TableApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.annotation.Generated;
+import javax.validation.constraints.*;
+
+import java.util.Optional;
+
+@Generated(
+    value = "org.openapitools.codegen.languages.SpringCodegen",
+    comments = "Generator version: 7.12.0")
+@Controller
+@RequestMapping("${openapi.lanceDBRESTCatalog.base-path:}")
+public class TableApiController implements TableApi {
+
+  private final NativeWebRequest request;
+
+  @Autowired
+  public TableApiController(NativeWebRequest request) {
+    this.request = request;
+  }
+
+  @Override
+  public Optional<NativeWebRequest> getRequest() {
+    return Optional.ofNullable(request);
+  }
+}
