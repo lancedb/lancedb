ab65d38c5438b4a5537b124c13fb81bf61e28c99
Will Jones <willjones127@gmail.com>
2025-06-04 11:38:13 -0700

    fix updating lockfile

--- ci/update_lockfiles.sh
+++ ci/update_lockfiles.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+AMEND=false
+
+for arg in "$@"; do
+  if [[ "$arg" == "--amend" ]]; then
+    AMEND=true
+  fi
+done
+
 # This updates the lockfile without building
 cargo metadata > /dev/null
 
@@ -13,7 +21,10 @@ popd
 
 if git diff --quiet --exit-code; then
   echo "No lockfile changes to commit; skipping amend."
-else
+else if $AMEND; then
   git add Cargo.lock nodejs/package-lock.json node/package-lock.json
   git commit --amend --no-edit
+else; then
+  git add Cargo.lock nodejs/package-lock.json node/package-lock.json
+  git commit -m "Update lockfiles"
 fi
