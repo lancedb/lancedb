edb971fc2b73d4d3ac1f2b78f7e76568e709b4f8
Weston Pace <weston.pace@gmail.com>
2025-06-06 10:13:47 -0700

    Cleanup how we pass in remote table

--- rust/lancedb/src/remote/table.rs
+++ rust/lancedb/src/remote/table.rs
@@ -32,6 +32,7 @@ use lance::dataset::{ColumnAlteration, NewColumnTransform, Version};
 use lance_datafusion::exec::{execute_plan, OneShotExec};
 use reqwest::{RequestBuilder, Response};
 use serde::{Deserialize, Serialize};
+use serde_json::Number;
 use std::collections::HashMap;
 use std::io::Cursor;
 use std::pin::Pin;
@@ -438,17 +439,15 @@ impl<S: HttpSend> RemoteTable<S> {
 
         // Apply general parameters, before we dispatch based on number of query vectors.
         body["distance_type"] = serde_json::json!(query.distance_type.unwrap_or_default());
+        // For backwards compatibility, newer clients should ignore this property
+        // as it is less precise.
+        body["nprobes"] = query.minimum_nprobes.into();
         if let Some(maximum_nprobes) = query.maximum_nprobes {
-            if query.minimum_nprobes == maximum_nprobes {
-                // For backwards compatibility, support older servers that only understand nprobes
-                // if we are able.
-                body["nprobes"] = query.minimum_nprobes.into();
-            }
-            body["minimum_nprobes"] = query.minimum_nprobes.into();
-            body["maximum_nprobes"] = query.maximum_nprobes.into();
+            body["maximum_nprobes"] = maximum_nprobes.into();
         } else {
-            body["minimum_nprobes"] = query.minimum_nprobes.into();
+            body["maximum_nprobes"] = serde_json::Value::Number(Number::from_u128(0).unwrap())
         }
+        body["minimum_nprobes"] = query.minimum_nprobes.into();
         body["lower_bound"] = query.lower_bound.into();
         body["upper_bound"] = query.upper_bound.into();
         body["ef"] = query.ef.into();
