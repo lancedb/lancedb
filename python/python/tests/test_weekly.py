dc1f276133f17889fb5d5c8b2a519e89ae2ee286
BubbleCal <bubble-cal@outlook.com>
2025-06-19 20:25:11 +0800

    add license header
    Signed-off-by: BubbleCal <bubble-cal@outlook.com>

--- python/python/tests/test_weekly.py
+++ python/python/tests/test_weekly.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright The LanceDB Authors
+
 # Weekly tests that runs all operations on a large dataset,
 # these operations are ran in random order repeated 10 times
 
