f076bb41f4c94273260044a97887b8b66596c06e
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2025-07-15 21:03:03 +0530

    feat: add support for returning all scores with rerankers (#2509)
    Previously `return_score="all"` was supported only for the default
    reranker (RRF) and not the model based rerankers.
    This adds support for keeping all scores in the base reranker so that
    all model based rerankers can use it. Its a slower path than keeping
    just the relevance score but can be useful in debugging

--- python/python/lancedb/rerankers/jinaai.py
+++ python/python/lancedb/rerankers/jinaai.py
@@ -97,14 +97,14 @@ class JinaReranker(Reranker):
         vector_results: pa.Table,
         fts_results: pa.Table,
     ):
-        combined_results = self.merge_results(vector_results, fts_results)
+        if self.score == "all":
+            combined_results = self._merge_and_keep_scores(vector_results, fts_results)
+        else:
+            combined_results = self.merge_results(vector_results, fts_results)
         combined_results = self._rerank(combined_results, query)
         if self.score == "relevance":
             combined_results = self._keep_relevance_score(combined_results)
-        elif self.score == "all":
-            raise NotImplementedError(
-                "return_score='all' not implemented for JinaReranker"
-            )
+
         return combined_results
 
     def rerank_vector(self, query: str, vector_results: pa.Table):
