131e5f9d047818ac65f1b0e6ad3ea654a3d799fb
Weston Pace <weston.pace@gmail.com>
2025-08-13 09:11:17 -0700

    More nodejs doctest fix

--- nodejs/examples/ann_indexes.test.ts
+++ nodejs/examples/ann_indexes.test.ts
@@ -13,7 +13,7 @@ test("ann index examples", async () => {
     const db = await lancedb.connect(databaseDir);
 
     const data = Array.from({ length: 1_000 }, (_, i) => ({
-      vector: Array(32).fill(i),
+      vector: Array(128).fill(i),
       id: `${i}`,
       content: "",
       longId: `${i}`,
@@ -24,8 +24,8 @@ test("ann index examples", async () => {
     });
     await table.createIndex("vector", {
       config: lancedb.Index.ivfPq({
-        numPartitions: 2,
-        numSubVectors: 2,
+        numPartitions: 30,
+        numSubVectors: 8,
       }),
     });
     // --8<-- [end:ingest]
