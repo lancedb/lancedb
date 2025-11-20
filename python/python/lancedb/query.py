bb6b0bea0ce7ca0b7bc2ec5f1ff4f82d679e2a56
Jackson Hew <50257390+jachewz@users.noreply.github.com>
2025-11-21 05:32:37 +1100

    fix: .phrase_query() not working (#2781)
    The `self._query` value was not set when wrapping its copy `query` with
    quotation marks.
    
    The test for phrase queries has been updated to test the
    `.phrase_query()` method as well, which will catch this bug.
    
    ---------
    
    Co-authored-by: Will Jones <willjones127@gmail.com>

--- python/python/lancedb/query.py
+++ python/python/lancedb/query.py
@@ -1495,7 +1495,7 @@ class LanceFtsQueryBuilder(LanceQueryBuilder):
         if self._phrase_query:
             if isinstance(query, str):
                 if not query.startswith('"') or not query.endswith('"'):
-                    query = f'"{query}"'
+                    self._query = f'"{query}"'
             elif isinstance(query, FullTextQuery) and not isinstance(
                 query, PhraseQuery
             ):
