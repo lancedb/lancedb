1840aa7edcdbf0c3f7d3a303000c8f75842a2f55
Will Jones <willjones127@gmail.com>
2026-01-13 11:23:14 -0800

    feat(rust)!: remove default features (#2912)
    BREAKING CHANGE: removes `aws`, `dynamodb`, `azure`, `gcs`, `oss`,
    `huggingface` from default Rust features. They can be enabled by users
    as needed.
    
    They are still enabled for Python and NodeJS, since those users don't
    control the compilation of artifacts.
    
    Closes #2911

--- rust/lancedb/src/lib.rs
+++ rust/lancedb/src/lib.rs
@@ -25,13 +25,14 @@
 //!
 //! ## Crate Features
 //!
-//! ### Experimental Features
-//!
-//! These features are not enabled by default.  They are experimental or in-development features that
-//! are not yet ready to be released.
-//!
-//! - `remote` - Enable remote client to connect to LanceDB cloud.  This is not yet fully implemented
-//!   and should not be enabled.
+//! - `aws` - Enable AWS S3 object store support.
+//! - `dynamodb` - Enable DynamoDB manifest store support.
+//! - `azure` - Enable Azure Blob Storage object store support.
+//! - `gcs` - Enable Google Cloud Storage object store support.
+//! - `oss` - Enable Alibaba Cloud OSS object store support.
+//! - `remote` - Enable remote client to connect to LanceDB cloud.
+//! - `huggingface` - Enable HuggingFace Hub integration for loading datasets from the Hub.
+//! - `fp16kernels` - Enable FP16 kernels for faster vector search on CPU.
 //!
 //! ### Quick Start
 //!
